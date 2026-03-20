6698f7bd10d61dce771af06c727d95168d99d7d2
cycloholic <konstantinidis.kon@yahoo.com>
2026-03-20 12:05:19 +0100

    Initial Commit

---
+++ src/main/java/com/app/pharmashift/PharmaShiftApplication.java
@@ -0,0 +1,13 @@
+package com.app.pharmashift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaShiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaShiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/pharmashift/PharmaShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pharmashift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaShiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
